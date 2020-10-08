8d779885899f21959d3f2ab9c29b39553cac729f
sam bacha <sam@freighttrust.com>
2020-10-08 05:08:29 -0700

    fix

--- setup_lua.sh
+++ setup_lua.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 sudo apt update 
+curl -R -O http://www.lua.org/ftp/lua-5.4.0.tar.gz
+tar zxf lua-5.4.0.tar.gz
+cd lua-5.4.0 || exit
+make all test
+cd $HOME || exit
 wget https://luarocks.org/releases/luarocks-3.3.1.tar.gz
 tar zxpf luarocks-3.3.1.tar.gz
-cd luarocks-3.3.1
+cd luarocks-3.3.1 || exit
 ./configure && make && sudo make install
 sudo luarocks install luasocket
 lua
