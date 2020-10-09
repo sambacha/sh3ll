830bbcc58b03fb8afaa028855e4f84570285e720
sam bacha <sam@freighttrust.com>
2020-10-09 13:00:08 -0700

    Create config-format.bash

---
+++ clubhouse/config-format.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Which configuration format do you prefer?"
+select config_extension in ".js" ".json" "Cancel"; do
+  case $config_extension in
+    .js ) config_opening='module.exports = {'; break;;
+    .json ) config_opening='{'; break;;
+    Cancel ) exit;;
+  esac
+done
+echo
